b874328ad6f53e0772a32e760d7c414bb5ebe8b5
Artem Chernyak <achernyak@pop-os.localdomain>
2019-07-18 19:04:48 -0500

    working xmonad config

---
+++ xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Actions.Volume
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50 
+            }    
+        , terminal = "gnome-terminal"
+        , modMask = mod4Mask
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        , ((mod4Mask, xK_F5), lowerVolume 4 >> return ())
+        , ((mod4Mask, xK_F6), raiseVolume 4 >> return ())
+        ]
