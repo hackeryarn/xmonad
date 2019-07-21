dff7f3a0225f2fb45eb2427f075c1af985b11882
hackeryarn <artemchernyak@gmail.com>
2019-07-21 03:04:48 -0500

    modified config for kde

---
+++ clone.sh
@@ -0,0 +1,4 @@
+git clone "https://github.com/xmonad/xmonad" xmonad-git
+git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
+git clone "https://github.com/jaor/xmobar" xmobar-git
+git clone "https://github.com/xmonad/xmonad-extras" xmonad-extras-git
